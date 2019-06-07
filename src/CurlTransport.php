2552d3099f7b237dbdc1d485bb2996fd9453b35a
Zsolt Fekete <zsolt.fekete@antavo.com>
2019-06-07 16:36:33 +0200

    [Request] CURLOPT_POST option can be hijacked via Transport::setOption().

--- src/CurlTransport.php
+++ src/CurlTransport.php
@@ -90,7 +90,10 @@ class CurlTransport implements TransportInterface {
         if (Request::isBodyAllowed($request->getMethod())) {
             $request->setBodySent($body = $request->encodeBody());
             curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
-            curl_setopt($ch, CURLOPT_POST, TRUE);
+
+            if (!$this->getOption(CURLOPT_POST)) {
+                curl_setopt($ch, CURLOPT_POST, TRUE);
+            }
         }
 
         $result = curl_exec($ch);
